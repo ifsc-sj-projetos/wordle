2869921fcddd3d9de1f43eb6523d3122e82b1963
sx-nt <ssemeghini.santana@gmail.com>
2025-02-09 18:19:56 -0300

    Criando e editando game stats, ainda preciso fazer funcionar.

--- src/wordle/stats/GameStats.java
+++ src/wordle/stats/GameStats.java
@@ -1,5 +1,13 @@
 package wordle.stats;
 
+import netscape.javascript.JSException;
+import netscape.javascript.JSObject;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.json.*;
+
 import java.util.Random;
 
 public class GameStats {
@@ -10,11 +18,62 @@ public class GameStats {
     protected int streak;
     protected int bestStreak;
 
+    private static final String FILE_PATH = "resources/stats.json";
+
     public GameStats() {
         loadStats();
     }
 
     private void loadStats() {
+
+        try {
+            String content = new String(Files.readAllBytes(Paths.get(FILE_PATH)));
+            JSONObject json = new JSONObject(content);
+
+            gamesPlayed = json.getInt("gamesPlayed");
+            gamesWon = json.getInt("gamesWon");
+            setVictoryPercent();
+            streak = json.getInt("currentStreak");
+            bestStreak = json.getInt("bestStreak");
+
+        } catch (IOException e) {
+            gamesPlayed = gamesWon = streak = bestStreak = 0;
+        }
+    }
+
+    public void setVictoryPercent() {
+        float percent = ((float) gamesWon / gamesPlayed) * 100;
+        victoryPercent = (int) percent;
+    }
+    public void updateStats(boolean won) {
+        gamesPlayed++;
+        if (won) {
+            gamesWon++;
+            streak++;
+            if (streak > bestStreak) {
+                bestStreak = streak;
+            }
+        } else {
+            streak = 0;
+        }
+        saveStats();
+    }
+
+    private void saveStats() {
+        try {
+            JSONObject json = new JSONObject();
+            json.put("gamesPlayed", gamesPlayed);
+            json.put("gamesWon", gamesWon);
+            json.put("streak", streak);
+            json.put("bestStreak", bestStreak);
+            Files.write(Paths.get(FILE_PATH), json.toString().getBytes());
+
+        } catch (IOException e) {
+            System.out.println("teste");
+        }
+    }
+
+    /*private void loadStats() {
 
         Random random = new Random();
 
@@ -29,5 +88,7 @@ public class GameStats {
         streak = Math.min(a, b);
         bestStreak = Math.max(a, b);
 
+    }*/
+
     }
-}
+
